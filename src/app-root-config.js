c9b69ec2930cec547ef0b91d867116e24b789fe6
Avinash Tiwari <vnshtiwari7@gmail.com>
2022-07-22 19:42:11 +0530

    new product repo

--- src/app-root-config.js
+++ src/app-root-config.js
@@ -2,29 +2,25 @@ import { registerApplication, start } from "single-spa";
 
 registerApplication({
   name: "@app/AppHeader",
-  app: () =>
-    System.import("@app/AppHeader"),
-  activeWhen: ["/"],
+  app: () => System.import("@app/AppHeader"),
+  activeWhen: ["/home"],
 });
 
 registerApplication({
   name: "@app/AppFooter",
-  app: () =>
-    System.import("@app/AppFooter"),
-  activeWhen: ["/"],
+  app: () => System.import("@app/AppFooter"),
+  activeWhen: ["/home"],
 });
 
 registerApplication({
   name: "@app/Home",
-  app: () =>
-    System.import("@app/Home"),
+  app: () => System.import("@app/Home"),
   activeWhen: ["/home"],
 });
 
 registerApplication({
   name: "@app/Products",
-  app: () =>
-    System.import("@app/Products"),
+  app: () => System.import("@app/Products"),
   activeWhen: ["/products"],
 });
 // registerApplication({
