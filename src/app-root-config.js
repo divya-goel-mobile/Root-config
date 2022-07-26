1784146fefd308222d16ebc827885de9c77c7008
Divya Goel <goel.divya.srms@gmail.com>
2022-07-26 12:04:42 +0530

    changed

--- src/app-root-config.js
+++ src/app-root-config.js
@@ -4,7 +4,7 @@ registerApplication({
   name: "@app/AppHeader",
   app: () => System.import("@app/AppHeader"),
   activeWhen: [
-    (location) => location.pathname === "/" && location.pathname != "/products",
+    (location) => location.pathname === "/" || location.pathname != "/products",
   ],
 });
 
@@ -12,26 +12,28 @@ registerApplication({
   name: "@app/AppFooter",
   app: () => System.import("@app/AppFooter"),
   activeWhen: [
-    (location) => location.pathname === "/" && location.pathname != "/products",
+    (location) => location.pathname === "/home" && location.pathname != "/products",
   ],
 });
 
-// registerApplication({
-//   name: "@app/Home",
-//   app: () => System.import("@app/Home"),
-//   activeWhen: ["/home"],
-// });
+registerApplication({
+  name: "@app/Home",
+  app: () => System.import("@app/Home"),
+  activeWhen: ["/home"],
+});
 
 registerApplication({
   name: "@app/Products",
   app: () => System.import("@app/Products"),
   activeWhen: ["/products"],
 });
-// registerApplication({
-//   name: "@app/navbar",
-//   app: () => System.import("@app/navbar"),
-//   activeWhen: ["/"]
-// });
+
+registerApplication({
+  name: "@app/ProductDetail",
+  app: () =>
+    System.import("@app/ProductDetail"),
+  activeWhen: ["/productDetail/:numberValue"],
+});
 
 start({
   urlRerouteOnly: true,
