5fa78fd281e703f5cb2c308ad3b7d2961583b969
Avinash Tiwari <vnshtiwari7@gmail.com>
2022-07-28 16:02:08 +0530

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = (webpackConfigEnv, argv) => {
   const orgName = "app";
@@ -14,6 +16,7 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+
     plugins: [
       new HtmlWebpackPlugin({
         inject: false,
@@ -22,6 +25,12 @@ module.exports = (webpackConfigEnv, argv) => {
           isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
           orgName,
         },
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: "importmap.json", to: "" },
+          { from: "staticwebapp.config.json", to: "" },
+        ],
       }),
     ],
   });
